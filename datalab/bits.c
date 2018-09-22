ba5ca67ce46aad0123281b3dc7c0d836bf82768c
Yuudachi_CivilEng <adsl2328606@gmail.com>
2018-09-22 11:14:09 +0800

    Update bits.c

--- datalab/bits.c
+++ datalab/bits.c
@@ -4,12 +4,22 @@
  *   Legal ops: ~ |
  *   Max ops: 8
  *   Rating: 1
- */
-/*
  * 用非与或运算实现与运算，热身题。
  */
 int bitAnd(int x, int y) {
   return ~(~x|~y);
 }
 
+/* 
+ * getByte - Extract byte n from word x
+ *   Bytes numbered from 0 (LSB) to 3 (MSB)
+ *   Examples: getByte(0x12345678,1) = 0x56
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 2
+ * 表示一个16进制数需要4bit也就是半个字节，则原数每右移1字节需要右移8位,即右移8n位
+ */
+int getByte(int x, int n) {
+  return (x >> (n << 3)) & (0xFF);
+}
 
