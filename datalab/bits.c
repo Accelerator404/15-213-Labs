a1af42e2a8b9e356a97125a11797ddbb73eb49dc
Yuudachi_CivilEng <adsl2328606@gmail.com>
2018-09-22 10:56:24 +0800

    Update bits.c

--- datalab/bits.c
+++ datalab/bits.c
@@ -1,1 +1,15 @@
+/* 
+ * bitAnd - x&y using only ~ and | 
+ *   Example: bitAnd(6, 5) = 4
+ *   Legal ops: ~ |
+ *   Max ops: 8
+ *   Rating: 1
+ */
+/*
+ * 用非与或运算实现与运算，热身题。
+ */
+int bitAnd(int x, int y) {
+  return ~(~x|~y);
+}
+
 
